f7e413de40b18562351602a55aaace145c5765de
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-06-26 21:20:18 +0100

    completed hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,35 @@
 import { motion } from "framer-motion";
-
+// import { hi } from "../assets";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
+        
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
+        <div className='w-5 h-5 rounded-full bg-[#FFC0CB]' />
+          <div className='w-1 sm:h-80 h-40 pink-gradient' />
           
         </div>
 
-        <div>
+        <div className="grid grid-cols-2 gap-4" >
+          <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#FFC0CB]'>Iman</span>
+            I'm <span className='text-[#FFC0CB]'>Iman</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm a Doctor and <br className='sm:block hidden' />
-            full stack Developer
+            I'm a full stack <br className='sm:block hidden' />Developer
           </p>
+          </div>
+          <div className="flex-1 flex items-center justify-center h-full">
+        <img src='/src/assets/hi.png' alt="" className="md:w-11/12 object-scale-down h-80 w-96" />
+      </div>
         </div>
+        
       </div>
 
       <ComputersCanvas />
