c15284f2a5064a889b72e7cb23e4d507a6f291ac
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-06-27 23:11:04 +0100

    fixed errors

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,23 +10,8 @@ import doctor from "./doctor.png"
 import forecastly from "./forecastly.png"
 import ecommerce from './ecommerce.png'
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+
 import nhs from "./company/nhs.png";
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import MovieSQL from "./MovieSQL.png"
 import readme from './readme.png'
 
@@ -40,23 +25,7 @@ export {
   github,
   menu,
   close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
   nhs,
-  carrent,
-  jobit,
-  tripguide,
   doctor,
   MovieSQL,
   forecastly,
